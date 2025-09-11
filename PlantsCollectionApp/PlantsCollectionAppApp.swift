fb593338d851a18a37e3179be8fbb18da14cca0d
Hadeel <Hadeel.Salman.al@gmail.com>
2025-09-11 22:46:21 +0300

    Initial Commit

---
+++ PlantsCollectionApp/PlantsCollectionAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  PlantsCollectionAppApp.swift
+//  PlantsCollectionApp
+//
+//  Created by Hadeel on 11/09/2025.
+//
+
+import SwiftUI
+
+@main
+struct PlantsCollectionAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
