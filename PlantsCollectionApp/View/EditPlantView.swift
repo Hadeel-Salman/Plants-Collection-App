ebaddd58f02025694c02732eb7c2dc7650379476
Hadeel <Hadeel.Salman.al@gmail.com>
2025-09-16 23:54:32 +0300

    adding the ability to edit

---
+++ PlantsCollectionApp/View/EditPlantView.swift
@@ -0,0 +1,18 @@
+//
+//  EditPlantView.swift
+//  PlantsCollectionApp
+//
+//  Created by Hadeel on 16/09/2025.
+//
+
+import SwiftUI
+
+struct EditPlantView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    EditPlantView()
+}
